0195b3144d569f22580d20d33c38590a83d413cc
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-11-08 17:00:54 +0900

    html all

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,4 +1,7 @@
 class DiariesController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new, :create]
+  before_action :move_to_index, only: :show
   
   def new
     @diary = Diary.new
@@ -25,6 +28,13 @@ class DiariesController < ApplicationController
     private
     def diary_params
       params.require(:diary).permit(:image, :title, :text, :day).merge(user_id: current_user.id)
+    end
+
+    def move_to_index
+      @diary = Diary.find(params[:id])
+      unless user_signed_in? && current_user.id == @diary.user_id
+        redirect_to root_path
+      end
     end
   
 end
