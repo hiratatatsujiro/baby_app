f57a14565256a18400a6632be70988cae9355453
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-11-14 16:41:31 +0900

    child models system

--- spec/system/child_spec.rb
+++ spec/system/child_spec.rb
@@ -1,93 +1,77 @@
 require 'rails_helper'
 
-RSpec.describe 'ユーザー新規登録', type: :system do
+RSpec.describe '子ども新規登録', type: :system do
   before do
-    @child = FactoryBot.build(:child)
+    @child = FactoryBot.create(:child)
   end
 
-  context 'ユーザー新規登録ができるとき' do 
-    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+  context '子ども新規登録ができるとき' do 
+    it '正しい情報を入力すれば新規登録ができてトップページに移動する' do
       # トップページに移動する
       visit root_path
       # トップページにサインアップページへ遷移するボタンがあることを確認する
-      expect(page).to have_content('新規ユーザー登録')
-      # 新規登録ページへ移動する
-      sign_in(@user)
-      # トップページへ遷移することを確認する
-      # ログアウトボタンが表示されることを確認する
-      expect(page).to have_content('ログアウト')
-      # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
-      expect(page).to have_no_content('新規ユーザー登録')
-      expect(page).to have_no_content('ログイン')
-    end
-  end
-
-  context 'ユーザー新規登録ができないとき' do
-    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
-      # トップページに移動する
-      visit root_path
-      # トップページにサインアップページへ遷移するボタンがあることを確認する
-      expect(page).to have_content('新規ユーザー登録')
-      # 新規登録ページへ移動する
-      visit new_user_registration_path
-      # ユーザー情報を入力する
-      fill_in 'user[name]', with: ""
-      fill_in 'user[email]', with: ""
-      fill_in 'user[password]', with: ""
-      fill_in 'user[password_confirmation]', with: ""
-      select "ーー", from: 'user[prefectures_id]'
-      fill_in 'user[city]', with: ""
-      fill_in 'user[house_number]', with: ""
-      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
-      expect{
-        find('input[name="commit"]').click
-      }.to change { User.count }.by(0)
-      expect(current_path).to eq(user_registration_path)
-    end
-  end
-end
-
-RSpec.describe 'ログイン', type: :system do
-  before do
-    @user = FactoryBot.create(:user)
-  end
-  context 'ログインができるとき' do
-    it '保存されているユーザーの情報と合致すればログインができる' do
-      # トップページに移動する
-      visit root_path
-      # トップページにログインページへ遷移するボタンがあることを確認する
       expect(page).to have_content('ログイン')
-      # ログインページへ遷移する
       visit new_user_session_path
       # 正しいユーザー情報を入力する
-      fill_in 'user[email]', with: @user.email
-      fill_in 'user[password]', with: @user.password
-      # ログインボタンを押す
+      fill_in 'user[email]', with: @child.user.email
+      fill_in 'user[password]', with: @child.user.password
       find('input[name="commit"]').click
-      # トップページへ遷移することを確認する
       expect(current_path).to eq(root_path)
       # ログアウトボタンが表示されることを確認する
-      expect(page).to have_content('ログアウト')
+      expect(page).to have_content('子どもの登録')
+      visit new_child_path
+      fill_in 'child[name]', with: @child.name
+      image_path = Rails.root.join('public/images/sample1.png')
+      attach_file('child[image]',image_path, make_visible: true)
+      select @child.gender.name, from: 'child[gender_id]'
+      select 2021 ,from: 'child[birthday(1i)]'
+      select 4 ,from: 'child[birthday(2i)]'
+      select 4 ,from: 'child[birthday(3i)]'
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Child.count }.by(1)
+      expect(current_path).to eq(root_path)
       # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
-      expect(page).to have_no_content('新規ユーザー登録')
-      expect(page).to have_no_content('ログイン')
+      expect(page).to have_content(@child.name)
     end
   end
-  context 'ログインができないとき' do
-    it '保存されているユーザーの情報と合致しないとログインができない' do
-      # トップページに移動する
-      visit root_path
-      # トップページにログインページへ遷移するボタンがあることを確認する
-      expect(page).to have_content('ログイン')
-      # ログインページへ遷移する
-      visit new_user_session_path
-      # ユーザー情報を入力する
-      fill_in 'user[email]', with: ""
-      fill_in 'user[password]', with: ""
-      # ログインボタンを押す
-      find('input[name="commit"]').click
-      # ログインページへ戻されることを確認する
-      expect(current_path).to eq(new_user_session_path)
+
+  context '子どもの登録ができないとき'do
+    it 'ログインしていないと投稿ページに遷移できない' do
+     # トップページにいる
+     visit root_path
+     # 学級通信一覧ページへのボタンがあることを確認する
+     expect(page).to have_content('子どもの登録')
+     visit new_child_path
+     expect(current_path).to eq(new_user_session_path)
     end
+ end
+end
+
+RSpec.describe '子ども詳細', type: :system do
+  before do
+    @child = FactoryBot.create(:child)
+  end
+  it 'ログインしたユーザーは子ども詳細ページに遷移して通院登録ボタンが表示される' do
+    # ログインする
+    visit root_path
+    # トップページにサインアップページへ遷移するボタンがあることを確認する
+    expect(page).to have_content('ログイン')
+    visit new_user_session_path
+    # 正しいユーザー情報を入力する
+    fill_in 'user[email]', with: @child.user.email
+    fill_in 'user[password]', with: @child.user.password
+    find('input[name="commit"]').click
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content(@child.name)
+    visit child_path(@child)
+    expect(page).to have_content("通院登録")
+    end
+  it 'ログインしていない状態で詳細ページに遷移できない' do
+    # トップページに移動する
+    visit root_path
+    # 学級通信一覧ページへのボタンがあることを確認する
+    expect(page).to have_no_content(@child.name)
   end
 end
+
