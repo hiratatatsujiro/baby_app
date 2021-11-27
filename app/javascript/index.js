b7121f0929b527729c6482513d98021b0bf209c7
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-11-27 16:51:43 +0900

    index code

---
+++ app/javascript/index.js
@@ -0,0 +1,16 @@
+function index() {
+  const menu_bar = document.getElementById("menu_bar");
+  const menu_box = document.getElementById("menu_box");
+
+  menu_bar.addEventListener("click", ()=>{
+    if (menu_box.getAttribute("")) {
+
+    } else {
+
+    }
+    menu_box.setAttribute()
+  });x
+}
+
+
+window.addEventListener("load", index);
