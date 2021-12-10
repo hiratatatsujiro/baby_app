36aa01722ca594845c9631623c27d469d833b086
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2021-12-11 03:55:51 +0900

    preview

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ require("../ajax_medical")
 require("../preview_diary")
 require("../preview_child")
 require("../preview_medical")
+require("../preview")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
