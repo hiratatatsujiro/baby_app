f2e416ac48715b09c5099c0b67d7e4a8a1de0575
hiratatatsujiro <i_am_3312in_meizen@yahoo.co.jp>
2022-01-23 07:47:33 +0900

    error

--- app/javascript/preview_medical.js
+++ app/javascript/preview_medical.js
@@ -76,4 +76,35 @@ document.addEventListener("DOMContentLoaded", function(){
     const fileField = document.querySelector("input[type='file'][name='medical[images][]']");
 
     fileField.addEventListener("change", changedFileField);
+  const fileField = document.querySelector("input[type='file'][name='medical[images][]']");
+  fileField.addEventListener("change", function(e){
+
+    const dataIndex = e.target.getAttribute('data-index');
+    console.log(dataIndex)
+
+    alreadyImage = document.querySelector(".preview");
+    if (alreadyImage) {
+      alreadyImage.remove();
+    }
+    const file = e.target.files[0];
+    const image = window.URL.createObjectURL(file);
+
+    const parent = document.createElement("div");
+    parent.setAttribute("class", "preview");
+    parent.setAttribute("data-index", dataIndex);
+
+    const child = document.createElement("img");
+    child.setAttribute("class", "preview-image");
+    child.setAttribute("src", image);
+    parent.appendChild(child);
+    previewSpace.appendChild(parent);
+
+    const secondImage = document.createElement("input");
+    secondImage.setAttribute("type", "file");
+    secondImage.setAttribute("name", "medical[images][]");
+
+    const secondImageSpace =  document.querySelector(".make-image");
+    secondImageSpace.appendChild(secondImage);
+
+  });
 });
